024efeb3514018be3db2a290cc6eb169c5bd1efa
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2025-09-29 13:52:27 +0300

    caesar

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,7 @@
-const caesarCipher = (str1, str2) => 
-    
-    (module.exports = caesarCipher);
+// CONSTATNS
+const UPPER_A = 65; // ASCII - A
+const UPPER_Z = 90; // ASCII - Z
+const LOWER_A = 97; // ASCII - a
+const ALPHABET_SIZE = 26;
+
+module.exports = caesarCipher;
